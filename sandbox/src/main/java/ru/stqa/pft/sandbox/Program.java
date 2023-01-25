5ab55a57c9fc45cb9de19846b8e29be7ad67f7d8
YaroslavnaMelko <melko@lanit.ru>
2023-01-25 21:59:11 +0300

    Добавлены функции

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Program.java
@@ -0,0 +1,24 @@
+package ru.stqa.pft.sandbox;
+
+public class Program {
+	public static void main (String[] args) {
+		hello("world");
+		hello("user");
+
+		double len = 5;
+		System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("Площадь прямоугольника со стороной " + a + " и " + b+ " = " + area(a,b));
+	}
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+	}
+	public static double area(double l) {
+		return l * l;
+	}
+	public static double area(double a, double b) {
+		return a*b;
+	}
+}
